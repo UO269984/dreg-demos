6ff0b3f73754384f1ce5f0d9717a1a5424173584
UO269984 <UO269984@uniovi.es>
2022-07-05 21:40:26 +0200

    ProgressBar SetColor fix

--- Vehicle-demo-unity/Assets/Scripts/UI/ProgressBar.cs
+++ Vehicle-demo-unity/Assets/Scripts/UI/ProgressBar.cs
@@ -9,16 +9,22 @@ public class ProgressBar : MonoBehaviour {
 	private RectTransform progressBox;
 	private Image progressImg;
 	
+	private Color color = Color.black;
+	
 	public void Start() {
 		this.progressBarBox = GetComponent<RectTransform>();
 		
 		GameObject progressObj = transform.GetChild(0).gameObject;
 		this.progressBox = progressObj.GetComponent<RectTransform>();
 		this.progressImg = progressObj.GetComponent<Image>();
+		this.progressImg.color = this.color;
 	}
 	
 	public void SetColor(Color color) {
-		this.progressImg.color = color;
+		this.color = color;
+		
+		if (this.progressImg != null)
+			this.progressImg.color = this.color;
 	}
 	
 	public void SetProgress(float percent) {
